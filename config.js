551f3847637f2aa81142cab02184926c4794d42d
alex <60557419+alex-git1@users.noreply.github.com>
2021-12-06 16:18:43 +1100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     username: "username",
     oauth: "oauth:abcdefghijklmnopqrstuvwxyz", // Get your token from here >> https://twitchapps.com/tmi/
-    keyPressDelay: 500, // 500 miliseconds (0.5 seconds)
+    keyHoldDelay: 500, // 500 miliseconds (0.5 seconds)
 };
